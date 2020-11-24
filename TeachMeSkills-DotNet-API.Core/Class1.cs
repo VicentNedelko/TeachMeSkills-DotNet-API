6559d6656b1eb60ac727c226bfae1fe51d28d1ae
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-11-24 22:09:01 +0300

    Add project files.

---
+++ TeachMeSkills-DotNet-API.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TeachMeSkills_DotNet_API.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ TeachMeSkills-DotNet-API.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeachMeSkills_DotNet_API.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
