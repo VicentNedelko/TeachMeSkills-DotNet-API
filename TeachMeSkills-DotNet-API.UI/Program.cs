be2ff443b453ebac0d0b9e8e19dec076bde8d817
VicentNedelko <glory.jly@gmail.com>
2020-11-30 15:11:14 +0300

    fix: fix syntax mistake

--- TeachMeSkills-DotNet-API.UI/Program.cs
+++ TeachMeSkills-DotNet-API.UI/Program.cs
@@ -13,6 +13,7 @@ namespace TeachMeSkills_DotNet_API.UI
     class Program
     {
         static void Main(string[] args)
+        {
             RequestService reqService = new RequestService();
             Parameters commons = new Parameters();
             while (true)
